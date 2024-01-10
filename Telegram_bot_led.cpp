5131d1e0b05322f629d243d22112799828eb8939
Angelo <angelo99franciamore@gmail.com>
2024-01-10 10:43:31 +0100

    added new project with telegram bot
    a telegrambot in which using the /led_on or /led_off it's possibile to on or off a led

---
+++ Telegram_bot_led.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <UniversalTelegramBot.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>  
+
+// Replace with your network credentials
+const char* ssid = "wifi";
+const char* password = "password";
+
+//Setup
+#define BOTtoken "6705920992:AAFVOSVhtfRlzr_CmyrL4h2Fzgrjq_iJK_k"  // @ESP8266_ang_bot
+#define CHAT_ID "40234663" // Token taken from @myidbot
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+// Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+//Setting pin
+const int ledPin = D5;
+bool ledState = LOW;
+
+// Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("Ricevuto nuovo messaggio");
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Utente non autorizzato", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    String from_name = bot.messages[i].from_name;
+
+    if (text == "/start") {
+      String welcome = "Welcome, " + from_name + ".\n";
+      welcome += "Usa questi 2 comandi per controllare i pin GPIO\n\n";
+      welcome += "/led_on accenderà il led \n";
+      welcome += "/led_off spegnerà il led \n";
+      welcome += "/stato stamperà lo stato del LED \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+
+    if (text == "/led_on") {
+      bot.sendMessage(chat_id, "Il LED è acceso", "");
+      ledState = HIGH;
+      digitalWrite(ledPin, ledState);
+    }
+    
+    if (text == "/led_off") {
+      bot.sendMessage(chat_id, "Il LED è spento", "");
+      ledState = LOW;
+      digitalWrite(ledPin, ledState);
+    }
+    
+    if (text == "/stato") {
+      if (digitalRead(ledPin)){
+        bot.sendMessage(chat_id, "LED is ON", "");
+      }
+      else{
+        bot.sendMessage(chat_id, "LED is OFF", "");
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  client.setInsecure();
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  } 
+}
